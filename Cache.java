d5135fbe8fd568e067e0e8fa835b32b378f36906
NishantAcharya <acharyanishant13@gmail.com>
2021-03-14 17:03:58 -0700

    Fied Reading from cache delay

--- Cache.java
+++ Cache.java
@@ -71,7 +71,7 @@ public class Cache extends Memory {
                 }
             }
 
-            return super.read(callingFrom, tagLoc + offset);
+            return super.readCache(callingFrom, tagLoc + offset);
         } else { // Cache miss
             // Read from next memory, wait if needed
             int[] line = nextMemory.getLine(callingFrom, address);
